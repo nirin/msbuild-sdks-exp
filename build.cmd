437cb11c09cae6fc9c679fe9dea2f255d05eb7db
Nirmal Guru <Nirmal4G@gmail.com>
2021-12-11 09:54:41 +0530

    Fixed some casing and spelling mistakes
    - Fixed Casing of SDK across files
    - Fixed Spelling in props and tasks files

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ set MSBuildSDKsPath=%RepoDir%\Source
 
 set "BuildFile=%~dp1"
 if "%BuildFile%" == "" (
-	set BuildFile=MSBuild-Sdks.sln
+	set BuildFile=MSBuild-SDKs.sln
 )
 
 if "%Configuration%" == "" set Configuration=Release
